8d4698ed2fa6a0fa38c1fe5f8ad85c702fe9f5db
viveknigam3003 <viveknigam.nigam3@gmail.com>
2021-04-19 17:11:46 +0530

    Added Custom Fonts

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,13 @@
-import "../styles/globals.css";
-import * as React from "react";
 import { ChakraProvider } from "@chakra-ui/react";
+import "@fontsource/inter";
+import "@fontsource/manrope";
+import * as React from "react";
+import "../styles/globals.css";
+import theme from "../styles/theme";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Component {...pageProps} />
     </ChakraProvider>
   );
