93113c77befe5d3a362d274a63c7c02f8e99b9a1
viveknigam3003 <viveknigam.nigam3@gmail.com>
2021-04-19 18:59:30 +0530

    Added Footer Links

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,23 +1,34 @@
-import { Box } from "@chakra-ui/layout";
+import { Box, Flex, HStack, Link, Spacer } from "@chakra-ui/layout";
 import React from "react";
+import { FooterLinks } from "../data/Links";
 import Author from "./Author";
 
 const Footer = ({ author, authorImage, twitter }) => {
   return (
-    <Box
-      display="grid"
-      alignItems="flex-start"
-      justifyContent="space-between"
-      width="100%"
-      bg="blue"
-      p="5"
-    >
+    <Flex width="100%" p="8" flexWrap="wrap" background="blue">
       <Author name={author} image={authorImage} twitter={twitter} />
-      <Box aria-label="Links"></Box>
+      <Spacer />
+      <Links />
       <Box aria-label=""></Box>
-    </Box>
+    </Flex>
   );
 };
+
+const Links = () => (
+  <HStack aria-label="Links">
+    {FooterLinks.map((item) => (
+      <Link
+        color="blue.100"
+        fontSize="md"
+        fontWeight="medium"
+        isExternal
+        href={item.link}
+      >
+        {item.label}
+      </Link>
+    ))}
+  </HStack>
+);
 
 Footer.defaultProps = {
   author: "Vivek Nigam",
---
+++ data/Links.js
@@ -0,0 +1,10 @@
+export const FooterLinks = [
+  {
+    label: "GitHub",
+    link: "https://github.com/viveknigam3003/futur-ikigai",
+  },
+  {
+    label: "Privacy Policy",
+    link: "",
+  },
+];
