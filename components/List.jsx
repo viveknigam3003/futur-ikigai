4b0fa1c540b6f1cd33af0047a1e557acbd1d6b62
viveknigam3003 <viveknigam.nigam3@gmail.com>
2021-04-20 22:44:22 +0530

    Extended ListContainer

--- components/List.jsx
+++ components/List.jsx
@@ -1,8 +1,27 @@
+import { IconButton } from "@chakra-ui/button";
+import { useDisclosure } from "@chakra-ui/hooks";
 import { Input } from "@chakra-ui/input";
 import { Box, Text, VStack } from "@chakra-ui/layout";
+import { Tooltip } from "@chakra-ui/tooltip";
 import React from "react";
+import { FiInfo } from "react-icons/fi";
+import Info from "./Info";
 
-const ListContainer = ({ color, title, subtitle, list }) => {
+const ListContainer = ({
+  color,
+  title,
+  subtitle,
+  list,
+  infoContent = [
+    {
+      type: "p",
+      body: "It seems this section is empty or we could not fetch data for it.",
+    },
+  ],
+  infoTitle = "No info found",
+}) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   return (
     <Box
       p="8"
@@ -21,11 +40,28 @@ const ListContainer = ({ color, title, subtitle, list }) => {
         </Text>
         <Text color={`${color}.400`} fontSize="md">
           {subtitle}
+          <Tooltip label="Know more" aria-label="Know-More-Info">
+            <IconButton
+              icon={<FiInfo />}
+              isRound
+              size="sm"
+              variant="ghost"
+              onClick={onOpen}
+              color={`${color}.400`}
+            />
+          </Tooltip>
         </Text>
       </Box>
       <VStack>
         <Input borderColor={`${color}.400`} />
       </VStack>
+      <Info isOpen={isOpen} onClose={onClose} title={infoTitle}>
+        {infoContent.map((item, index) => (
+          <Text as={item.type} fontSize="sm" key={`${infoTitle}-${index}`}>
+            {item.body}
+          </Text>
+        ))}
+      </Info>
     </Box>
   );
 };
