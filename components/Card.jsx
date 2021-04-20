e70fe5db750a1c6de7b93d90d8e867b54ede1530
viveknigam3003 <viveknigam.nigam3@gmail.com>
2021-04-20 19:43:17 +0530

    Added NextJS Routing and Progress Bar

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,35 +1,36 @@
 import { useColorModeValue } from "@chakra-ui/color-mode";
-import { Box, Link, Text } from "@chakra-ui/layout";
+import { Box, Text } from "@chakra-ui/layout";
 import React from "react";
+import Link from "next/link";
 
 const Card = ({ overline, title, body, color, backgroundColor, path }) => {
   return (
-    <Box
-      as="a"
-      href={path}
-      m="1rem"
-      p="3rem"
-      display="flex"
-      justifyContent="center"
-      flexDirection="column"
-      height="inherit"
-      flexBasis={{ base: "100%", md: "35%", lg: "40%" }}
-      shadow={useColorModeValue("md", "dark-lg")}
-      borderRadius="10px"
-      transition="color 0.15s ease, background-color 0.15s ease, shadow 0.15s ease"
-      _hover={{ color, backgroundColor, shadow: "lg" }}
-      cursor="pointer"
-    >
-      <Text fontSize="sm" fontWeight="medium" textTransform="uppercase">
-        {overline}
-      </Text>
-      <Text fontSize="4xl" fontWeight="semibold" mb="4">
-        {title} &rarr;
-      </Text>
-      <Text fontSize="md" mt="1">
-        {body}
-      </Text>
-    </Box>
+    <Link href={path}>
+      <Box
+        m="1rem"
+        p="3rem"
+        display="flex"
+        justifyContent="center"
+        flexDirection="column"
+        height="inherit"
+        flexBasis={{ base: "100%", md: "35%", lg: "40%" }}
+        shadow={useColorModeValue("md", "dark-lg")}
+        borderRadius="10px"
+        transition="color 0.15s ease, background-color 0.15s ease, shadow 0.15s ease"
+        _hover={{ color, backgroundColor, shadow: "lg" }}
+        cursor="pointer"
+      >
+        <Text fontSize="sm" fontWeight="medium" textTransform="uppercase">
+          {overline}
+        </Text>
+        <Text fontSize="4xl" fontWeight="semibold" mb="4">
+          {title} &rarr;
+        </Text>
+        <Text fontSize="md" mt="1">
+          {body}
+        </Text>
+      </Box>
+    </Link>
   );
 };
 
--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,22 @@
+import ProgressBar from "@badrap/bar-of-progress";
 import { ChakraProvider } from "@chakra-ui/react";
 import "@fontsource/inter";
 import "@fontsource/manrope";
+import Router from "next/router";
 import * as React from "react";
 import "../styles/globals.css";
 import theme from "./theme";
+
+const progress = new ProgressBar({
+  size: 4,
+  color: theme.colors.blue[300],
+  className: {zIndex: 50},
+  delay: 80,
+});
+
+Router.events.on("routeChangeStart", progress.start);
+Router.events.on("routeChangeComplete", progress.finish);
+Router.events.on("routeChangeError", progress.finish);
 
 function MyApp({ Component, pageProps }) {
   return (
