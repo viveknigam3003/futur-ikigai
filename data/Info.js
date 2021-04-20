c30249bba7729c8cf247ef44500e720e2e49e1b4
viveknigam3003 <viveknigam.nigam3@gmail.com>
2021-04-20 22:44:09 +0530

    Added Info Data

---
+++ data/Info.js
@@ -0,0 +1,12 @@
+const InfoText = {
+  love: [
+    {
+      type: "p",
+      body:
+        "Things that you love can be under several categories as listed below",
+    },
+    { type: "li", body: "Things that are effortless" },
+  ],
+};
+
+export default InfoText;
