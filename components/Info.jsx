312a20814f587d2504c518a4a1b1d0bbe018c03a
viveknigam3003 <viveknigam.nigam3@gmail.com>
2021-04-20 22:43:06 +0530

    Added Info Component

---
+++ components/Info.jsx
@@ -0,0 +1,32 @@
+import {
+  Button,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+} from "@chakra-ui/react";
+import React from "react";
+
+const Info = ({ isOpen, onClose, title, children }) => {
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>{title}</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody>{children}</ModalBody>
+
+        <ModalFooter>
+          <Button colorScheme="blue" mr={3} onClick={onClose}>
+            Close
+          </Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
+};
+
+export default Info;
