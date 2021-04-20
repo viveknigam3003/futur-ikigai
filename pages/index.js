baaaee0c8ca1882e40bacf6db4860fbcd54c95c2
viveknigam3003 <viveknigam.nigam3@gmail.com>
2021-04-20 12:22:23 +0530

    Added Cards and Navbar

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,16 @@
-import { Text } from "@chakra-ui/layout";
+import { Box, Text } from "@chakra-ui/layout";
 import Card from "../components/Card";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import Meta from "../components/Meta";
+import Navbar from "../components/Navbar";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Meta title="Find your Ikigai" />
-
+      <Navbar />
       <main className={styles.main}>
         <Header overline="The Futur" title="Ikigai" subtitle="生きがい" />
 
@@ -17,29 +18,29 @@ export default function Home() {
           Ikigai stands for "a reason for being"
         </Text>
 
-        <div className={styles.grid}>
+        <Box
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          flexWrap="wrap"
+          maxWidth="64rem"
+          mt="3rem"
+        >
           <Card
+            overline="Step 1"
             title="Looking Inside"
             body="Discovering what you love, what you're good at, and what the world needs."
+            color="blue.500"
+            backgroundColor="blue.50"
           />
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-        </div>
+          <Card
+            overline="Step 2"
+            title="Finding Ikigai"
+            body="Crafting ideas and extracting ultimate creativity for finding your Ikigai"
+            color="orange.500"
+            backgroundColor="orange.50"
+          />
+        </Box>
       </main>
 
       <Footer />
