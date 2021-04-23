faa656bff7fc184f10943784c849b2bb0891fbc7
viveknigam3003 <viveknigam.nigam3@gmail.com>
2021-04-23 17:54:56 +0530

    Added Hints Dummy Data

---
+++ data/Hints.js
@@ -0,0 +1,11 @@
+const Hints = {
+    tangible: [
+      { type: "li", body: "Things that are effortless" },
+      { type: "li", body: "Things that are effortless" },
+      { type: "li", body: "Things that are effortless" },
+      { type: "li", body: "Things that are effortless" },
+    ],
+  };
+  
+  export default Hints;
+  
