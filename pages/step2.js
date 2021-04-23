5fcb50b47cbbea5710678244e6acae0b14acc711
viveknigam3003 <viveknigam.nigam3@gmail.com>
2021-04-23 17:20:22 +0530

    Added Step 2

--- pages/step2.js
+++ pages/step2.js
@@ -1,15 +1,46 @@
+import { Box, Text, VStack } from "@chakra-ui/layout";
 import React from "react";
 import StepLayout from "../components/StepLayout";
+import { generateCombo } from "../utils";
 
-const Step1 = () => {
+const Step2 = () => {
+  const lists = JSON.parse(localStorage.getItem("lists"));
+  const combos = generateCombo(lists);
+
+  const TextItem = ({ item, ...rest }) => (
+    <Box
+      p="2"
+      mr="2"
+      flex="1 1 auto"
+      textAlign="left"
+      shadow="base"
+      borderRadius="8px"
+      width="100%"
+      {...rest}
+    >
+      <Text>{item}</Text>
+    </Box>
+  );
+
+  const colorList = ["red", "yellow", "blue"]
+
   return (
     <StepLayout
       pageTitle="STEP 2: Finding Ikigai"
       navTitle="Finding Ikigai"
       navSubtitle="Crafting ideas to find your Ikigai."
       color="orange"
-    ></StepLayout>
+    >
+      <Box display="flex" justifyContent="space-evenly" width="100%">
+        <VStack>
+          {combos.map((item, index) => (
+            <TextItem item={item} key={item} color={`${colorList[index]}.800`} backgroundColor={`${colorList[index]}.100`}/>
+          ))}
+        </VStack>
+        
+      </Box>
+    </StepLayout>
   );
 };
 
-export default Step1;
+export default Step2;
