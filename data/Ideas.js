50df83c1fbe3e97fd989e52d93780d9c22a43d92
viveknigam3003 <viveknigam.nigam3@gmail.com>
2021-04-29 21:51:34 +0530

    Idea Data

---
+++ data/Ideas.js
@@ -0,0 +1,20 @@
+export const Ideas = [
+  {
+    id: "1",
+    name: "Idea #1",
+    combo: [],
+    idea: { tangible: "", digital: "", service: "" },
+  },
+  {
+    id: "2",
+    name: "Idea #2",
+    combo: [],
+    idea: { tangible: "", digital: "", service: "" },
+  },
+  {
+    id: "3",
+    name: "Idea #3",
+    combo: [],
+    idea: { tangible: "", digital: "", service: "" },
+  },
+];
