6e381a55b058cbaed87f14b730f574df4ba0b84a
viveknigam3003 <viveknigam.nigam3@gmail.com>
2021-05-01 14:22:25 +0530

    Updating VERCEL_URL

--- config/index.js
+++ config/index.js
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== "production";
 
-export const SERVER = dev ? "http://localhost:3000" : process.env.NEXT_PUBLIC_SERVER_URL;
+export const SERVER = dev ? "http://localhost:3000" : process.env.VERCEL_URL;
