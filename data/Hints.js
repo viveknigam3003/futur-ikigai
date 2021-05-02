bcf035e03e6ce2280562a06d19269184cc011f18
viveknigam3003 <viveknigam.nigam3@gmail.com>
2021-05-02 21:03:07 +0530

    Added Idea hints

--- data/Hints.js
+++ data/Hints.js
@@ -1,21 +1,42 @@
 const Hints = {
   tangible: [
-    { type: "li", body: "Things that are effortless" },
-    { type: "li", body: "Things that are effortless" },
-    { type: "li", body: "Things that are effortless" },
-    { type: "li", body: "Things that are effortless" },
+    {
+      type: "p",
+      body:
+        "Tangible - a thing that is perceptible by touch. Some examples maybe - ",
+    },
+    { type: "li", body: "A physical product" },
+    { type: "li", body: "Utility Item" },
+    { type: "li", body: "Novelty (a small and inexpensive toy or ornament)" },
+    { type: "li", body: "Printed - Books, posters, deck" },
+    { type: "li", body: "Manufactured products" },
+    { type: "li", body: "Accessories" },
+    { type: "li", body: "Fashion items" },
   ],
   digital: [
-    { type: "li", body: "Things that are effortless" },
-    { type: "li", body: "Things that are effortless" },
-    { type: "li", body: "Things that are effortless" },
-    { type: "li", body: "Things that are effortless" },
+    { type: "p", body: "Examples of digital items" },
+    { type: "li", body: "App" },
+    { type: "li", body: "Website" },
+    { type: "li", body: "Templates" },
+    { type: "li", body: "Assets" },
+    {
+      type: "li",
+      body: "Digital Assets (images, videos, designs, documents etc.)",
+    },
+    { type: "li", body: "Games" },
+    { type: "li", body: "E-Books" },
+    { type: "li", body: "Presets" },
+    { type: "li", body: "Stock art/sound/3-d elements" },
   ],
   service: [
-    { type: "li", body: "Things that are effortless" },
-    { type: "li", body: "Things that are effortless" },
-    { type: "li", body: "Things that are effortless" },
-    { type: "li", body: "Things that are effortless" },
+    {
+      type: "p",
+      body:
+        "A service would be the most common area to come up with business ideas.",
+    },
+    { type: "li", body: "It can be something you're hired to do" },
+    { type: "li", body: "Some way by which you can help other people" },
+    { type: "li", body: "Solving client problems" },
   ],
 };
 
