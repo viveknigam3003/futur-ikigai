ca2fba0bbb33c3bc312b3f03c4cb681ec4ede07e
viveknigam3003 <viveknigam.nigam3@gmail.com>
2021-05-02 16:49:54 +0530

    Added Reset Handling

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,38 @@
+import { useDisclosure } from "@chakra-ui/hooks";
 import { Box, Text } from "@chakra-ui/layout";
+import { MenuItem } from "@chakra-ui/menu";
+import { useToast } from "@chakra-ui/toast";
+import { Fragment } from "react";
+import ActionModal from "../components/ActionModal";
 import Card from "../components/Card";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import Meta from "../components/Meta";
 import Navbar from "../components/Navbar";
 import styles from "../styles/Home.module.css";
+import { IDEAS_KEY, LIST_KEY } from "../utils/constants";
 
 export default function Home() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const toast = useToast();
+
+  const handleReset = () => {
+    localStorage.removeItem(LIST_KEY);
+    localStorage.removeItem(IDEAS_KEY);
+
+    toast({
+      title: "Application Reset Complete.",
+      description: "You can now start over with Step 1",
+      status: "success",
+      duration: 9000,
+      isClosable: true,
+    });
+  };
+
   return (
     <div className={styles.container}>
       <Meta title="Find your Ikigai" />
-      <Navbar />
+      <Navbar menuItems={<MenuItems openResetDialog={onOpen} />} />
       <main className={styles.main}>
         <Header overline="The Futur" title="Ikigai" subtitle="生きがい" />
 
@@ -43,9 +65,28 @@ export default function Home() {
             path="/step2"
           />
         </Box>
+        <ActionModal
+          title="Are you sure you want to reset?"
+          isOpen={isOpen}
+          onClose={onClose}
+          onClick={handleReset}
+          buttonText="Confirm Reset"
+        >
+          Resetting the application will delete all its data on this device.
+          This action is irreversible. Do you wish to continue?
+        </ActionModal>
       </main>
 
       <Footer />
     </div>
   );
 }
+
+const MenuItems = ({ openResetDialog }) => (
+  <Fragment>
+    <MenuItem>Know More</MenuItem>
+    <MenuItem color="red.400" onClick={openResetDialog}>
+      Reset App
+    </MenuItem>
+  </Fragment>
+);
