f8cc8ee207c1edb943aac7f7bbff3014fc8ff130
viveknigam3003 <viveknigam.nigam3@gmail.com>
2021-05-02 16:47:49 +0530

    Added Menu

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,17 +1,21 @@
 import { IconButton } from "@chakra-ui/button";
 import { useColorMode } from "@chakra-ui/color-mode";
-import { Box, HStack, Link } from "@chakra-ui/layout";
+import { Box, HStack } from "@chakra-ui/layout";
+import { MenuButton } from "@chakra-ui/menu";
+import { MenuList } from "@chakra-ui/menu";
+import { Menu } from "@chakra-ui/menu";
 import { Tooltip } from "@chakra-ui/tooltip";
 import React from "react";
-import { FaGithub, FaMoon, FaSun } from "react-icons/fa";
+import { BsGearFill } from "react-icons/bs";
+import { FaMoon, FaSun } from "react-icons/fa";
 
-const Navbar = ({children}) => {
+const Navbar = ({ children, menuItems }) => {
   const { colorMode, toggleColorMode } = useColorMode();
   return (
     <Box
       display="flex"
       flexDir="row-reverse"
-      alignItems={{base:"flex-start", md: "center"}}
+      alignItems={{ base: "flex-start", md: "center" }}
       justifyContent="space-between"
       width="100%"
       p={{ base: "2rem 1.5rem 1rem 1.5rem", md: "1rem 3rem", lg: "1rem 4rem" }}
@@ -24,14 +28,10 @@ const Navbar = ({children}) => {
             variant="ghost"
           />
         </Tooltip>
-        <Tooltip label="GitHub Repo" aria-label="GitHub-Icon">
-          <Link
-            isExternal
-            href={`https://github.com/viveknigam3003/futur-ikigai`}
-          >
-            <IconButton icon={<FaGithub />} variant="ghost" />
-          </Link>
-        </Tooltip>
+        <Menu>
+          <MenuButton as={IconButton} icon={<BsGearFill />} variant="ghost" />
+          <MenuList>{menuItems}</MenuList>
+        </Menu>
       </HStack>
 
       {children}
