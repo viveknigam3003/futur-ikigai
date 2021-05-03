843ab0550fab64a614dfb8bfacf8458a78b1d284
viveknigam3003 <viveknigam.nigam3@gmail.com>
2021-05-03 19:20:17 +0530

    Updated SEO params

--- config/next-seo.config.js
+++ config/next-seo.config.js
@@ -1,17 +1,17 @@
 export default {
   title: "Find your Ikigai (The Futur X VN)",
   description:
-    "Find your Ikigai with the Chris Do (The Futur) method. This is a helper tool for following along with the Ikigai workshop by The Futur on Youtube.",
+    "Find your Ikigai with the Chris Do (The Futur) method. This is a helper tool for following along with the Ikigai workshop by The Futur on Youtube. Developed by Vivek Nigam (@_viveknigam_)",
   canonical: "https://futur-ikigai.vercel.app",
   openGraph: {
     url: "https://futur-ikigai.vercel.app/",
     title: "Find your Ikigai (The Futur X VN)",
     description:
-      "Find your Ikigai with the Chris Do (The Futur) method. This is a helper tool for following along with the Ikigai workshop by The Futur on Youtube.",
+      "Find your Ikigai with the Chris Do (The Futur) method. This is a helper tool for following along with the Ikigai workshop by The Futur on Youtube. Developed by Vivek Nigam (@_viveknigam_)",
     site_name: "Find your Ikigai",
     images: [
       {
-        url: "/the-futur-ikigai.jpeg",
+        url: "https://futur-ikigai.vercel.app/the-futur-ikigai.jpeg",
         alt: "Vivek Nigam & The Futur - Find your Ikigai",
       },
     ],
